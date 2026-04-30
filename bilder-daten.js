3e6e962f3d3faa8f3691177cf62a4c4c3aaf5117
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-04-30 18:52:01 +0200

    Aendere atico in desvan in Haus1

--- bilder-daten.js
+++ bilder-daten.js
@@ -344,7 +344,7 @@ window.APP_FOLDERS = {
         title: "La casa – Bild 6",
         file: "Haus1.png",
         hotspots: [
-          { wort: "el ático", clickX: 50, clickY: 18.5, labelX: 50, labelY: 12, areaW: 47, areaH: 20 },
+          { wort: "el desván", clickX: 50, clickY: 18.5, labelX: 50, labelY: 12, areaW: 47, areaH: 20 },
           { wort: "el cuarto de los niños", clickX: 29.8, clickY: 35, labelX: 28, labelY: 38, areaW: 30, areaH: 17 },
           { wort: "el dormitorio", clickX: 69.2, clickY: 35, labelX: 69, labelY: 38, areaW: 30, areaH: 17 },
           { wort: "el salón", clickX: 28.7, clickY: 52.2, labelX: 22, labelY: 56, areaW: 25, areaH: 18 },
