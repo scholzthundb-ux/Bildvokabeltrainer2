5fbc629dd42572cd4c4148e468208e9559b2a579
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-05-05 15:46:46 +0200

    Passe Reihen in Strand3 an

--- bilder-daten.js
+++ bilder-daten.js
@@ -173,15 +173,15 @@ window.APP_FOLDERS = {
         title: "La playa – Bild 9",
         file: "strand3.png",
         hotspots: [
-          { wort: "tomar el sol", clickX: 16.7, clickY: 16.7, labelX: 24, labelY: 9.5, areaW: 30, areaH: 28 },
-          { wort: "el nadador", clickX: 50, clickY: 16.7, labelX: 50, labelY: 9.5, areaW: 30, areaH: 28 },
-          { wort: "el buceador", clickX: 83.3, clickY: 16.7, labelX: 76, labelY: 9.5, areaW: 30, areaH: 28 },
-          { wort: "pasear por la playa", clickX: 16.7, clickY: 50, labelX: 28, labelY: 41.5, areaW: 30, areaH: 28 },
-          { wort: "hacer castillos de arena", clickX: 50, clickY: 50, labelX: 50, labelY: 41.5, areaW: 30, areaH: 28 },
-          { wort: "leer", clickX: 83.3, clickY: 50, labelX: 74, labelY: 41.5, areaW: 30, areaH: 28 },
-          { wort: "recoger conchas", clickX: 16.7, clickY: 83.3, labelX: 25, labelY: 75, areaW: 30, areaH: 28 },
-          { wort: "sacar fotos", clickX: 50, clickY: 83.3, labelX: 50, labelY: 75, areaW: 30, areaH: 28 },
-          { wort: "el surfista", clickX: 83.3, clickY: 83.3, labelX: 74, labelY: 75, areaW: 30, areaH: 28 }
+          { wort: "tomar el sol", clickX: 16.7, clickY: 21.2, labelX: 24, labelY: 9.5, areaW: 30, areaH: 28 },
+          { wort: "el nadador", clickX: 50, clickY: 21.2, labelX: 50, labelY: 9.5, areaW: 30, areaH: 28 },
+          { wort: "el buceador", clickX: 83.3, clickY: 21.2, labelX: 76, labelY: 9.5, areaW: 30, areaH: 28 },
+          { wort: "pasear por la playa", clickX: 16.7, clickY: 51.5, labelX: 28, labelY: 41.5, areaW: 30, areaH: 28 },
+          { wort: "hacer castillos de arena", clickX: 50, clickY: 51.5, labelX: 50, labelY: 41.5, areaW: 30, areaH: 28 },
+          { wort: "leer", clickX: 83.3, clickY: 51.5, labelX: 74, labelY: 41.5, areaW: 30, areaH: 28 },
+          { wort: "recoger conchas", clickX: 16.7, clickY: 83.3, labelX: 25, labelY: 75, areaW: 30, areaH: 26 },
+          { wort: "sacar fotos", clickX: 50, clickY: 83.3, labelX: 50, labelY: 75, areaW: 30, areaH: 26 },
+          { wort: "el surfista", clickX: 83.3, clickY: 83.3, labelX: 74, labelY: 75, areaW: 30, areaH: 26 }
         ]
       },
       {
