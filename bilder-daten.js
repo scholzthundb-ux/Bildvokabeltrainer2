8de830dbe0097264019fc3faa2abe56e65659315
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-05-05 14:31:32 +0200

    Fuege Strand9 hinter Strand8 ein

--- bilder-daten.js
+++ bilder-daten.js
@@ -141,6 +141,21 @@ window.APP_FOLDERS = {
       },
       {
         title: "La playa – Bild 7",
+        file: "strand9.png",
+        hotspots: [
+          { wort: "el salvavidas", clickX: 16.7, clickY: 16.7, labelX: 24, labelY: 9.5, areaW: 30, areaH: 31 },
+          { wort: "la tabla de surf", clickX: 50, clickY: 16.7, labelX: 50, labelY: 9.5, areaW: 30, areaH: 31 },
+          { wort: "la bandera roja", clickX: 83.3, clickY: 16.7, labelX: 76, labelY: 9.5, areaW: 30, areaH: 31 },
+          { wort: "la pala", clickX: 16.7, clickY: 50, labelX: 24, labelY: 42, areaW: 30, areaH: 31 },
+          { wort: "el cubo", clickX: 50, clickY: 50, labelX: 50, labelY: 42, areaW: 30, areaH: 31 },
+          { wort: "la pelota", clickX: 83.3, clickY: 50, labelX: 76, labelY: 42, areaW: 30, areaH: 31 },
+          { wort: "el flotador", clickX: 16.7, clickY: 83.3, labelX: 24, labelY: 75, areaW: 30, areaH: 31 },
+          { wort: "la concha", clickX: 50, clickY: 83.3, labelX: 50, labelY: 75, areaW: 30, areaH: 31 },
+          { wort: "el castillo de arena", clickX: 83.3, clickY: 83.3, labelX: 76, labelY: 75, areaW: 30, areaH: 31 }
+        ]
+      },
+      {
+        title: "La playa – Bild 8",
         file: "strand3.png",
         hotspots: [
           { wort: "tomar el sol", clickX: 16.7, clickY: 16.7, labelX: 24, labelY: 9.5, areaW: 30, areaH: 31 },
@@ -155,7 +170,7 @@ window.APP_FOLDERS = {
         ]
       },
       {
-        title: "La playa – Bild 8",
+        title: "La playa – Bild 9",
         file: "strand4.png",
         hotspots: [
           { wort: "hacer snorkel", clickX: 16.7, clickY: 16.7, labelX: 24, labelY: 9.5, areaW: 30, areaH: 31 },
