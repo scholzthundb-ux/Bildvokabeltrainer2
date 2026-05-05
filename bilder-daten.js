5e9860fbd94e8399e926ecdf25c0aa556081e412
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-05-05 15:24:29 +0200

    Passe Reihen in Strand7 an

--- bilder-daten.js
+++ bilder-daten.js
@@ -113,15 +113,15 @@ window.APP_FOLDERS = {
         title: "La playa – Bild 5",
         file: "strand7.png",
         hotspots: [
-          { wort: "el chiringuito", clickX: 16.7, clickY: 16.7, labelX: 24, labelY: 9.5, areaW: 30, areaH: 28 },
-          { wort: "la sombrilla", clickX: 50, clickY: 16.7, labelX: 50, labelY: 9.5, areaW: 30, areaH: 28 },
-          { wort: "la tumbona", clickX: 83.3, clickY: 16.7, labelX: 76, labelY: 9.5, areaW: 30, areaH: 28 },
-          { wort: "la toalla", clickX: 16.7, clickY: 50, labelX: 24, labelY: 42, areaW: 30, areaH: 28 },
-          { wort: "las gafas de sol", clickX: 50, clickY: 50, labelX: 50, labelY: 42, areaW: 30, areaH: 28 },
-          { wort: "la crema solar", clickX: 83.3, clickY: 50, labelX: 76, labelY: 42, areaW: 30, areaH: 28 },
-          { wort: "el sombrero de sol", clickX: 16.7, clickY: 83.3, labelX: 24, labelY: 75, areaW: 30, areaH: 28 },
-          { wort: "el bolso de playa", clickX: 50, clickY: 83.3, labelX: 50, labelY: 75, areaW: 30, areaH: 28 },
-          { wort: "la nevera portátil", clickX: 83.3, clickY: 83.3, labelX: 76, labelY: 75, areaW: 30, areaH: 28 }
+          { wort: "el chiringuito", clickX: 16.7, clickY: 21.2, labelX: 24, labelY: 9.5, areaW: 30, areaH: 28 },
+          { wort: "la sombrilla", clickX: 50, clickY: 21.2, labelX: 50, labelY: 9.5, areaW: 30, areaH: 28 },
+          { wort: "la tumbona", clickX: 83.3, clickY: 21.2, labelX: 76, labelY: 9.5, areaW: 30, areaH: 28 },
+          { wort: "la toalla", clickX: 16.7, clickY: 50.8, labelX: 24, labelY: 42, areaW: 30, areaH: 26 },
+          { wort: "las gafas de sol", clickX: 50, clickY: 50.8, labelX: 50, labelY: 42, areaW: 30, areaH: 26 },
+          { wort: "la crema solar", clickX: 83.3, clickY: 50.8, labelX: 76, labelY: 42, areaW: 30, areaH: 26 },
+          { wort: "el sombrero de sol", clickX: 16.7, clickY: 81.8, labelX: 24, labelY: 75, areaW: 30, areaH: 28 },
+          { wort: "el bolso de playa", clickX: 50, clickY: 81.8, labelX: 50, labelY: 75, areaW: 30, areaH: 28 },
+          { wort: "la nevera portátil", clickX: 83.3, clickY: 81.8, labelX: 76, labelY: 75, areaW: 30, areaH: 28 }
         ]
       },
       {
