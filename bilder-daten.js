fe474d8aa02b7222cc4f4c83d9d9fe337cf2a18e
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-05-06 01:04:36 +0200

    Aktualisiere ropa6 und ropa7

--- bilder-daten.js
+++ bilder-daten.js
@@ -300,20 +300,6 @@ window.APP_FOLDERS = {
         title: "La ropa – Bild 6",
         file: "ropa6.png",
         hotspots: [
-          { wort: "el traje", clickX: 16.7, clickY: 19.5, labelX: 24, labelY: 9.5, areaW: 30, areaH: 28 },
-          { wort: "el traje de chaqueta", clickX: 50, clickY: 19.5, labelX: 50, labelY: 9.5, areaW: 30, areaH: 28 },
-          { wort: "el chándal", clickX: 83.3, clickY: 19.5, labelX: 76, labelY: 9.5, areaW: 30, areaH: 28 },
-          { wort: "el uniforme", clickX: 16.7, clickY: 50, labelX: 24, labelY: 42, areaW: 30, areaH: 28 },
-          { wort: "el disfraz", clickX: 50, clickY: 50, labelX: 50, labelY: 42, areaW: 30, areaH: 28 },
-          { wort: "el mono", clickX: 83.3, clickY: 50, labelX: 76, labelY: 42, areaW: 30, areaH: 28 },
-          { wort: "la americana", clickX: 16.7, clickY: 81.8, labelX: 24, labelY: 75, areaW: 30, areaH: 28 },
-          { wort: "el vestido de novia", clickX: 50, clickY: 81.8, labelX: 50, labelY: 75, areaW: 30, areaH: 28 }
-        ]
-      },
-      {
-        title: "La ropa – Bild 7",
-        file: "ropa7.png",
-        hotspots: [
           { wort: "el abrigo", clickX: 16.7, clickY: 19.5, labelX: 24, labelY: 9.5, areaW: 30, areaH: 28 },
           { wort: "el cortavientos", clickX: 50, clickY: 19.5, labelX: 50, labelY: 9.5, areaW: 30, areaH: 28 },
           { wort: "el chubasquero", clickX: 83.3, clickY: 19.5, labelX: 76, labelY: 9.5, areaW: 30, areaH: 28 },
@@ -323,6 +309,20 @@ window.APP_FOLDERS = {
           { wort: "el cárdigan", clickX: 16.7, clickY: 81.8, labelX: 24, labelY: 75, areaW: 30, areaH: 28 },
           { wort: "el anorak", clickX: 50, clickY: 81.8, labelX: 50, labelY: 75, areaW: 30, areaH: 28 },
           { wort: "la chaqueta de plumas", clickX: 83.3, clickY: 81.8, labelX: 76, labelY: 75, areaW: 30, areaH: 28 }
+        ]
+      },
+      {
+        title: "La ropa – Bild 7",
+        file: "ropa7.png",
+        hotspots: [
+          { wort: "el traje", clickX: 16.7, clickY: 19.5, labelX: 24, labelY: 9.5, areaW: 30, areaH: 28 },
+          { wort: "el traje de chaqueta", clickX: 50, clickY: 19.5, labelX: 50, labelY: 9.5, areaW: 30, areaH: 28 },
+          { wort: "el chándal", clickX: 83.3, clickY: 19.5, labelX: 76, labelY: 9.5, areaW: 30, areaH: 28 },
+          { wort: "el uniforme", clickX: 16.7, clickY: 50, labelX: 24, labelY: 42, areaW: 30, areaH: 28 },
+          { wort: "el disfraz", clickX: 50, clickY: 50, labelX: 50, labelY: 42, areaW: 30, areaH: 28 },
+          { wort: "el mono", clickX: 83.3, clickY: 50, labelX: 76, labelY: 42, areaW: 30, areaH: 28 },
+          { wort: "la americana", clickX: 16.7, clickY: 81.8, labelX: 24, labelY: 75, areaW: 30, areaH: 28 },
+          { wort: "el vestido de novia", clickX: 50, clickY: 81.8, labelX: 50, labelY: 75, areaW: 30, areaH: 28 }
         ]
       }
     ]
