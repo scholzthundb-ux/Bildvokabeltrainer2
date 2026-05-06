85df8267980da76d7b3300527fbf59d36539ce6b
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-05-06 22:26:21 +0200

    Erweitere Hotspots in desvan1

--- bilder-daten.js
+++ bilder-daten.js
@@ -698,13 +698,13 @@ window.APP_FOLDERS = {
         hotspots: [
           { wort: "la viga", clickX: 17.5, clickY: 8.8, labelX: 25, labelY: 6.5, areaW: 11, areaH: 8 },
           { wort: "la telaraña", clickX: 19.3, clickY: 27.3, labelX: 28, labelY: 23, areaW: 10, areaH: 9 },
-          { wort: "la lámpara", clickX: 51.0, clickY: 30.8, labelX: 59, labelY: 24.5, areaW: 10, areaH: 9 },
+          { wort: "la lámpara", clickX: 51.0, clickY: 30.8, labelX: 59, labelY: 24.5, areaW: 14, areaH: 13 },
           { wort: "la claraboya", clickX: 84.0, clickY: 20.0, labelX: 77.5, labelY: 14, areaW: 11, areaH: 10 },
           { wort: "la caja", clickX: 17.0, clickY: 53.3, labelX: 24.5, labelY: 49.5, areaW: 11, areaH: 9 },
           { wort: "la silla plegable", clickX: 86.8, clickY: 60.5, labelX: 78.5, labelY: 55.5, areaW: 11, areaH: 10 },
           { wort: "el baúl", clickX: 42.3, clickY: 81.3, labelX: 51, labelY: 76.5, areaW: 12, areaH: 10 },
           { wort: "la escalera", clickX: 24.0, clickY: 94.0, labelX: 17, labelY: 88, areaW: 12, areaH: 10 },
-          { wort: "la maleta", clickX: 74.3, clickY: 94.0, labelX: 82, labelY: 88, areaW: 12, areaH: 10 }
+          { wort: "la maleta", clickX: 74.3, clickY: 94.0, labelX: 82, labelY: 88, areaW: 18, areaH: 15 }
         ]
       },
       {
