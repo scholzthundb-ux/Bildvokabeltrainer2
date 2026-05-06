9f069d426a5d96b2dbccd5067c340c2110404af3
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-05-06 22:53:52 +0200

    Ändere ropa zu ropa vieja

--- bilder-daten.js
+++ bilder-daten.js
@@ -732,7 +732,7 @@ window.APP_FOLDERS = {
           { wort: "los adornos de Navidad", clickX: 16.8, clickY: 50.0, labelX: 16.8, labelY: 42.5, areaW: 30, areaH: 31 },
           { wort: "el armario", clickX: 50.0, clickY: 50.0, labelX: 50.0, labelY: 42.5, areaW: 30, areaH: 31 },
           { wort: "las herramientas", clickX: 83.2, clickY: 50.0, labelX: 83.2, labelY: 42.5, areaW: 30, areaH: 31 },
-          { wort: "la ropa", clickX: 16.8, clickY: 81.7, labelX: 16.8, labelY: 74.2, areaW: 30, areaH: 31 },
+          { wort: "la ropa vieja", clickX: 16.8, clickY: 81.7, labelX: 16.8, labelY: 74.2, areaW: 30, areaH: 31 },
           { wort: "la manta", clickX: 50.0, clickY: 81.7, labelX: 50.0, labelY: 74.2, areaW: 30, areaH: 31 },
           { wort: "el fantasma", clickX: 83.2, clickY: 81.7, labelX: 83.2, labelY: 74.2, areaW: 30, areaH: 31 }
         ]
