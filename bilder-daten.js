68a3d38a199742f696a5faa6aa75a5dadaea2bb7
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-05-08 16:52:15 +0200

    Fuege ninos13 hinzu

--- bilder-daten.js
+++ bilder-daten.js
@@ -1134,6 +1134,21 @@ window.APP_FOLDERS = {
           { wort: "el móvil", clickX: 50, clickY: 83.3, labelX: 50, labelY: 75, areaW: 30, areaH: 28 },
           { wort: "la radio", clickX: 83.3, clickY: 83.3, labelX: 76, labelY: 75, areaW: 30, areaH: 28 }
         ]
+      },
+      {
+        title: "La habitación infantil – Bild 13",
+        file: "ninos13.png",
+        hotspots: [
+          { wort: "el televisor", clickX: 16.7, clickY: 21.2, labelX: 24, labelY: 9.5, areaW: 30, areaH: 28 },
+          { wort: "el teclado musical", clickX: 50, clickY: 21.2, labelX: 50, labelY: 9.5, areaW: 30, areaH: 28 },
+          { wort: "la guitarra", clickX: 83.3, clickY: 21.2, labelX: 76, labelY: 9.5, areaW: 30, areaH: 28 },
+          { wort: "los pósteres", clickX: 16.7, clickY: 51.5, labelX: 24, labelY: 42, areaW: 30, areaH: 28 },
+          { wort: "las gafas de realidad virtual", clickX: 50, clickY: 51.5, labelX: 50, labelY: 42, areaW: 30, areaH: 28 },
+          { wort: "la consola", clickX: 83.3, clickY: 51.5, labelX: 76, labelY: 42, areaW: 30, areaH: 28 },
+          { wort: "el mando", clickX: 16.7, clickY: 83.3, labelX: 24, labelY: 75, areaW: 30, areaH: 28 },
+          { wort: "los videojuegos", clickX: 50, clickY: 83.3, labelX: 50, labelY: 75, areaW: 30, areaH: 28 },
+          { wort: "la mochila escolar", clickX: 83.3, clickY: 83.3, labelX: 76, labelY: 75, areaW: 30, areaH: 28 }
+        ]
       }
     ]
   }
