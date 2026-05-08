195d176de36e4e18166ba3b7b9586afd2ee94f10
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-05-08 12:52:29 +0200

    Verschiebe erste Reihe in ninos4 erneut nach oben

--- bilder-daten.js
+++ bilder-daten.js
@@ -984,9 +984,9 @@ window.APP_FOLDERS = {
         title: "La habitación infantil – Bild 3",
         file: "ninos4.png",
         hotspots: [
-          { wort: "el armario", clickX: 16.7, clickY: 18.6, labelX: 24, labelY: 9.5, areaW: 30, areaH: 31 },
-          { wort: "la cómoda", clickX: 50, clickY: 18.6, labelX: 50, labelY: 9.5, areaW: 30, areaH: 31 },
-          { wort: "la cuna", clickX: 83.3, clickY: 18.6, labelX: 76, labelY: 9.5, areaW: 30, areaH: 31 },
+          { wort: "el armario", clickX: 16.7, clickY: 17.8, labelX: 24, labelY: 9.5, areaW: 30, areaH: 31 },
+          { wort: "la cómoda", clickX: 50, clickY: 17.8, labelX: 50, labelY: 9.5, areaW: 30, areaH: 31 },
+          { wort: "la cuna", clickX: 83.3, clickY: 17.8, labelX: 76, labelY: 9.5, areaW: 30, areaH: 31 },
           { wort: "la cuna mecedora", clickX: 16.7, clickY: 52.2, labelX: 24, labelY: 42, areaW: 30, areaH: 31 },
           { wort: "el parque para bebés", clickX: 50, clickY: 52.2, labelX: 50, labelY: 42, areaW: 30, areaH: 31 },
           { wort: "el cambiador", clickX: 83.3, clickY: 52.2, labelX: 76, labelY: 42, areaW: 30, areaH: 31 },
