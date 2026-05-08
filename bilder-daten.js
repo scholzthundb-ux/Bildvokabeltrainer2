ef826b6558df0562f8096983678a7cebeab793c3
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-05-08 11:39:49 +0200

    Verschiebe erste Reihe in ninos7

--- bilder-daten.js
+++ bilder-daten.js
@@ -1029,9 +1029,9 @@ window.APP_FOLDERS = {
         title: "La habitación infantil – Bild 6",
         file: "ninos7.png",
         hotspots: [
-          { wort: "el rompecabezas", clickX: 16.7, clickY: 21.2, labelX: 24, labelY: 9.5, areaW: 30, areaH: 28 },
-          { wort: "el libro ilustrado", clickX: 50, clickY: 21.2, labelX: 50, labelY: 9.5, areaW: 30, areaH: 28 },
-          { wort: "el libro para colorear", clickX: 83.3, clickY: 21.2, labelX: 76, labelY: 9.5, areaW: 30, areaH: 28 },
+          { wort: "el rompecabezas", clickX: 16.7, clickY: 19.5, labelX: 24, labelY: 9.5, areaW: 30, areaH: 28 },
+          { wort: "el libro ilustrado", clickX: 50, clickY: 19.5, labelX: 50, labelY: 9.5, areaW: 30, areaH: 28 },
+          { wort: "el libro para colorear", clickX: 83.3, clickY: 19.5, labelX: 76, labelY: 9.5, areaW: 30, areaH: 28 },
           { wort: "la caja de juguetes", clickX: 16.7, clickY: 51.5, labelX: 24, labelY: 42, areaW: 30, areaH: 28 },
           { wort: "el móvil de cuna", clickX: 50, clickY: 51.5, labelX: 50, labelY: 42, areaW: 30, areaH: 28 },
           { wort: "el caballito balancín", clickX: 83.3, clickY: 51.5, labelX: 76, labelY: 42, areaW: 30, areaH: 28 },
