7384dc98d33b69e9acf3c525a3d9421a248effb5
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-05-08 17:10:53 +0200

    Passe Hotspots von ninos14 an

--- bilder-daten.js
+++ bilder-daten.js
@@ -1154,12 +1154,12 @@ window.APP_FOLDERS = {
         title: "La habitación infantil – Bild 14",
         file: "ninos14.png",
         hotspots: [
-          { wort: "el libro", clickX: 16.7, clickY: 21.2, labelX: 24, labelY: 9.5, areaW: 30, areaH: 28 },
-          { wort: "el papel", clickX: 50, clickY: 21.2, labelX: 50, labelY: 9.5, areaW: 30, areaH: 28 },
-          { wort: "el bolígrafo", clickX: 83.3, clickY: 21.2, labelX: 76, labelY: 9.5, areaW: 30, areaH: 28 },
-          { wort: "el cuaderno", clickX: 16.7, clickY: 51.5, labelX: 24, labelY: 42, areaW: 30, areaH: 28 },
-          { wort: "los rotuladores", clickX: 50, clickY: 51.5, labelX: 50, labelY: 42, areaW: 30, areaH: 28 },
-          { wort: "el estuche", clickX: 83.3, clickY: 51.5, labelX: 76, labelY: 42, areaW: 30, areaH: 28 },
+          { wort: "el libro", clickX: 16.7, clickY: 18.8, labelX: 24, labelY: 9.5, areaW: 30, areaH: 32 },
+          { wort: "el papel", clickX: 50, clickY: 18.8, labelX: 50, labelY: 9.5, areaW: 30, areaH: 32 },
+          { wort: "el bolígrafo", clickX: 83.3, clickY: 18.8, labelX: 76, labelY: 9.5, areaW: 30, areaH: 32 },
+          { wort: "el cuaderno", clickX: 16.7, clickY: 52.8, labelX: 24, labelY: 42, areaW: 30, areaH: 31 },
+          { wort: "los rotuladores", clickX: 50, clickY: 52.8, labelX: 50, labelY: 42, areaW: 30, areaH: 31 },
+          { wort: "el estuche", clickX: 83.3, clickY: 52.8, labelX: 76, labelY: 42, areaW: 30, areaH: 31 },
           { wort: "la regla", clickX: 16.7, clickY: 83.3, labelX: 24, labelY: 75, areaW: 30, areaH: 28 },
           { wort: "las tijeras", clickX: 50, clickY: 83.3, labelX: 50, labelY: 75, areaW: 30, areaH: 28 },
           { wort: "la calculadora", clickX: 83.3, clickY: 83.3, labelX: 76, labelY: 75, areaW: 30, areaH: 28 }
