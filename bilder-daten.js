5990f0dfec115e8c83e4805cf8ff2cb46bcff90b
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-05-09 10:26:58 +0200

    Kuerze obere Reihe in bano3

--- bilder-daten.js
+++ bilder-daten.js
@@ -1050,9 +1050,9 @@ window.APP_FOLDERS = {
         title: "El baño – Bild 3",
         file: "bano3.png",
         hotspots: [
-          { wort: "la ducha", clickX: 16.8, clickY: 18.3, labelX: 16.8, labelY: 10.8, areaW: 30, areaH: 31 },
-          { wort: "la alcachofa de la ducha", clickX: 50.0, clickY: 18.3, labelX: 50.0, labelY: 10.8, areaW: 30, areaH: 31 },
-          { wort: "la grifería de la ducha", clickX: 83.2, clickY: 18.3, labelX: 83.2, labelY: 10.8, areaW: 30, areaH: 31 },
+          { wort: "la ducha", clickX: 16.8, clickY: 18.3, labelX: 16.8, labelY: 10.8, areaW: 30, areaH: 29 },
+          { wort: "la alcachofa de la ducha", clickX: 50.0, clickY: 18.3, labelX: 50.0, labelY: 10.8, areaW: 30, areaH: 29 },
+          { wort: "la grifería de la ducha", clickX: 83.2, clickY: 18.3, labelX: 83.2, labelY: 10.8, areaW: 30, areaH: 29 },
           { wort: "la mampara", clickX: 16.8, clickY: 48.5, labelX: 16.8, labelY: 41.0, areaW: 30, areaH: 31 },
           { wort: "la cortina de ducha", clickX: 50.0, clickY: 48.5, labelX: 50.0, labelY: 41.0, areaW: 30, areaH: 31 },
           { wort: "el váter", clickX: 83.2, clickY: 48.5, labelX: 83.2, labelY: 41.0, areaW: 30, areaH: 31 },
