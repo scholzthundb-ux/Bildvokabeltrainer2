28c4af1b7ac4845c575673c47de1d0582e9c3fcf
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-05-09 11:06:27 +0200

    Fuege lavadero3 hinzu

--- bilder-daten.js
+++ bilder-daten.js
@@ -1202,6 +1202,21 @@ window.APP_FOLDERS = {
           { wort: "el fregadero", clickX: 50.0, clickY: 83.0, labelX: 50.0, labelY: 75.5, areaW: 31, areaH: 31 },
           { wort: "el grifo", clickX: 83.2, clickY: 83.0, labelX: 83.2, labelY: 75.5, areaW: 31, areaH: 31 }
         ]
+      },
+      {
+        title: "El lavadero – Bild 3",
+        file: "lavadero3.png",
+        hotspots: [
+          { wort: "las pinzas de la ropa", clickX: 16.8, clickY: 18.3, labelX: 16.8, labelY: 10.8, areaW: 31, areaH: 31 },
+          { wort: "la tabla de planchar", clickX: 50.0, clickY: 18.3, labelX: 50.0, labelY: 10.8, areaW: 31, areaH: 31 },
+          { wort: "la plancha", clickX: 83.2, clickY: 18.3, labelX: 83.2, labelY: 10.8, areaW: 31, areaH: 31 },
+          { wort: "el cesto de la ropa", clickX: 16.8, clickY: 50.0, labelX: 16.8, labelY: 42.5, areaW: 31, areaH: 31 },
+          { wort: "la ropa sucia", clickX: 50.0, clickY: 50.0, labelX: 50.0, labelY: 42.5, areaW: 31, areaH: 31 },
+          { wort: "la percha", clickX: 83.2, clickY: 50.0, labelX: 83.2, labelY: 42.5, areaW: 31, areaH: 31 },
+          { wort: "el cubo de plástico", clickX: 16.8, clickY: 81.7, labelX: 16.8, labelY: 74.2, areaW: 31, areaH: 31 },
+          { wort: "las palanganas de plástico", clickX: 50.0, clickY: 81.7, labelX: 50.0, labelY: 74.2, areaW: 31, areaH: 31 },
+          { wort: "el recogedor", clickX: 83.2, clickY: 81.7, labelX: 83.2, labelY: 74.2, areaW: 31, areaH: 31 }
+        ]
       }
     ]
   },
