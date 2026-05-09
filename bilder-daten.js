1fa348e39fdc295a3dee02968af18be9162bd72f
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-05-09 10:29:58 +0200

    Passe weitere Hotspots in lavadero1 an

--- bilder-daten.js
+++ bilder-daten.js
@@ -1177,14 +1177,14 @@ window.APP_FOLDERS = {
         file: "lavadero1.png",
         openLabel: { wort: "el lavadero", labelX: 50, labelY: 9 },
         hotspots: [
-          { wort: "la secadora", clickX: 14.5, clickY: 36.5, labelX: 20.5, labelY: 30.5, areaW: 21, areaH: 23 },
-          { wort: "la lavadora", clickX: 13.5, clickY: 68.5, labelX: 18.5, labelY: 66.0, areaW: 22, areaH: 24 },
-          { wort: "el detergente", clickX: 30.0, clickY: 38.5, labelX: 36.0, labelY: 34.0, areaW: 10, areaH: 15 },
-          { wort: "el suavizante", clickX: 37.8, clickY: 40.5, labelX: 43.5, labelY: 36.0, areaW: 9, areaH: 13 },
+          { wort: "la secadora", clickX: 14.5, clickY: 38.0, labelX: 20.5, labelY: 31.5, areaW: 21, areaH: 23 },
+          { wort: "la lavadora", clickX: 15.5, clickY: 66.5, labelX: 19.5, labelY: 63.5, areaW: 24, areaH: 24 },
+          { wort: "el detergente", clickX: 30.0, clickY: 36.5, labelX: 36.0, labelY: 32.0, areaW: 10, areaH: 15 },
+          { wort: "el suavizante", clickX: 37.8, clickY: 38.3, labelX: 43.5, labelY: 34.0, areaW: 9, areaH: 13 },
           { wort: "el tendedero", clickX: 58.0, clickY: 42.5, labelX: 64.0, labelY: 38.5, areaW: 28, areaH: 18 },
           { wort: "el fregadero", clickX: 88.0, clickY: 49.0, labelX: 80.0, labelY: 45.5, areaW: 20, areaH: 10 },
-          { wort: "la plancha", clickX: 78.5, clickY: 61.0, labelX: 72.0, labelY: 57.5, areaW: 12, areaH: 14 },
-          { wort: "la tabla de planchar", clickX: 59.0, clickY: 72.5, labelX: 55.0, labelY: 68.5, areaW: 35, areaH: 13 },
+          { wort: "la plancha", clickX: 76.0, clickY: 59.0, labelX: 70.0, labelY: 55.5, areaW: 12, areaH: 14 },
+          { wort: "la tabla de planchar", clickX: 61.5, clickY: 70.5, labelX: 57.0, labelY: 66.5, areaW: 31, areaH: 13 },
           { wort: "el cesto de la ropa", clickX: 55.0, clickY: 86.5, labelX: 46.0, labelY: 82.5, areaW: 22, areaH: 15 }
         ]
       },
