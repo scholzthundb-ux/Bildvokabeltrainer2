3cd4184a24107ef7a20b06196e9ec3dfa89f1a6b
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-05-16 10:21:26 +0200

    Fuege cocina13 hinzu

--- bilder-daten.js
+++ bilder-daten.js
@@ -956,6 +956,21 @@ window.APP_FOLDERS = {
       },
       {
         title: "Los utensilios de la cocina – Bild 4",
+        file: "cocina13.png",
+        hotspots: [
+          { wort: "la pinza de azúcar", clickX: 16.8, clickY: 18.3, labelX: 16.8, labelY: 10.8, areaW: 30, areaH: 31 },
+          { wort: "el salero", clickX: 50.0, clickY: 18.3, labelX: 50.0, labelY: 10.8, areaW: 30, areaH: 31 },
+          { wort: "la bandeja", clickX: 83.2, clickY: 18.3, labelX: 83.2, labelY: 10.8, areaW: 30, areaH: 31 },
+          { wort: "el pimentero", clickX: 16.8, clickY: 50.0, labelX: 16.8, labelY: 42.5, areaW: 30, areaH: 31 },
+          { wort: "la panera", clickX: 50.0, clickY: 50.0, labelX: 50.0, labelY: 42.5, areaW: 30, areaH: 31 },
+          { wort: "la aceitera", clickX: 83.2, clickY: 50.0, labelX: 83.2, labelY: 42.5, areaW: 30, areaH: 31 },
+          { wort: "la vinagrera", clickX: 16.8, clickY: 81.7, labelX: 16.8, labelY: 74.2, areaW: 30, areaH: 31 },
+          { wort: "la botella", clickX: 50.0, clickY: 81.7, labelX: 50.0, labelY: 74.2, areaW: 30, areaH: 31 },
+          { wort: "el termo", clickX: 83.2, clickY: 81.7, labelX: 83.2, labelY: 74.2, areaW: 30, areaH: 31 }
+        ]
+      },
+      {
+        title: "Los utensilios de la cocina – Bild 5",
         file: "cocina10.png",
         hotspots: [
           { wort: "el temporizador", clickX: 16.8, clickY: 18.3, labelX: 16.8, labelY: 10.8, areaW: 30, areaH: 31 },
