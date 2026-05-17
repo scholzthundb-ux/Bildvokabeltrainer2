1c3489745857639cdbf178e33de928b38d737602
Birgit Scholz <birgitscholz@MacBookPro.fritz.box>
2026-05-17 04:15:03 +0200

    Passe balda in salon2 an

--- bilder-daten.js
+++ bilder-daten.js
@@ -728,7 +728,7 @@ window.APP_FOLDERS = {
           { wort: "el balcón", clickX: 16.0, clickY: 33.0, labelX: 23.5, labelY: 27.0, areaW: 17, areaH: 33 },
           { wort: "el cuadro", clickX: 59.0, clickY: 40.0, labelX: 65.0, labelY: 34.0, areaW: 17, areaH: 17 },
           { wort: "la planta", clickX: 43.0, clickY: 18.5, labelX: 50.0, labelY: 13.0, areaW: 15, areaH: 20 },
-          { wort: "la balda / el estante", clickX: 82.5, clickY: 24.0, labelX: 74.5, labelY: 18.5, areaW: 35, areaH: 10 },
+          { wort: "la balda", clickX: 82.5, clickY: 24.0, labelX: 74.5, labelY: 18.5, areaW: 35, areaH: 10 },
           { wort: "los libros", clickX: 61.5, clickY: 19.0, labelX: 68.5, labelY: 13.5, areaW: 20, areaH: 15 },
           { wort: "el cojín", clickX: 12.0, clickY: 74.0, labelX: 20.0, labelY: 67.5, areaW: 14, areaH: 14 },
           { wort: "el cajón", clickX: 77.0, clickY: 71.0, labelX: 83.5, labelY: 65.0, areaW: 17, areaH: 14 },
